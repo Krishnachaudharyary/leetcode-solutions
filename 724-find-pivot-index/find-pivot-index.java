f23c5a2dfa2a20d396d98df6162d35c4df0a614a
Krishna chaudhary <kcjaat2512@gmail.com>
2026-09-05 22:49:59 +0530

    Time: 1 ms (97.12%) | Memory: 47.4 MB (60.11%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n=nums.length;
+        int tsum=0;
+        int lsum=0;
+        for(int num:nums){
+            tsum+=num;
+        }
+        for(int i=0;i<n;i++){
+            
+            if(lsum==tsum-lsum-nums[i]){
+                return i;
+            }
+            
+            lsum=lsum+nums[i];
+            
+        }
+        return -1;
+    }
+}
